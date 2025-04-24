53c7d345f2456d439a2517464bb87223a3c9e0ba
maheshrenuakarapu <maheshrenu00@gmail.com>
2025-04-24 12:56:12 +0530

    Update StartEventScripterRemote.bat

--- StartEventScripterRemote.bat
+++ StartEventScripterRemote.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File ".\StartEventScripterRemote.ps1"
+powershell -ExecutionPolicy Bypass -File "C:\actions-runner\_work\demo_ES\demo_ES\StartEventScripterRemote.ps1"
 pause
